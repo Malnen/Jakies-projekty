e90eeff4d3ec04ec8b858242303ede7e396bd993
Marcel Niedziela <46851853+Malnen@users.noreply.github.com>
2020-05-07 14:43:01 +0200

    Add files via upload

---
+++ Strategia/src/strategia/Sorter.java
@@ -0,0 +1,142 @@
+package strategia;
+
+import java.util.Arrays;
+import java.util.Random;
+import java.util.concurrent.TimeUnit;
+
+public class Sorter {
+
+    public void sortArray(int array[]) {
+        strategy.sort(array);
+    }
+
+    public void setSortStrategy(SortStrategy strategy) {
+        this.strategy = strategy;
+    }
+    private SortStrategy strategy;
+
+    public void printArray(int array[]) {
+        for (int i : array) {
+            System.out.print(i + " ");
+        }
+        System.out.println("");
+    }
+
+    public void testOnRandomArrays() {
+        System.out.println("Sortowanie QuickSort");
+        setSortStrategy(new QuickSortStrategy());
+        sortOnRandom(10, 10);
+        sortOnRandom(10, 100);
+        sortOnRandom(10, 1000);
+        sortOnRandom(10, 10000);
+        sortOnRandom(10, 100000);
+        sortOnRandom(10, 1000000);
+
+        System.out.println("");
+
+        System.out.println("Sortowanie Arrays.sort()");
+        setSortStrategy(new JavaSortStrategy());
+        sortOnRandom(10, 10);
+        sortOnRandom(10, 100);
+        sortOnRandom(10, 1000);
+        sortOnRandom(10, 10000);
+        sortOnRandom(10, 100000);
+        sortOnRandom(10, 1000000);
+
+        System.out.println("");
+
+        System.out.println("Sortowanie BubbleSort");
+        setSortStrategy(new BubbleSortStrategy());
+        sortOnRandom(10, 10);
+        sortOnRandom(10, 100);
+        sortOnRandom(10, 1000);
+        sortOnRandom(10, 10000);
+        sortOnRandom(10, 100000); // okolo 13 sekund
+        sortOnRandom(10, 1000000); // okolo 130 sekund
+
+        System.out.println("");
+
+        System.out.println("Sortowanie SelectionSort");
+        setSortStrategy(new SelectionSortStrategy());
+        sortOnRandom(10, 10);
+        sortOnRandom(10, 100);
+        sortOnRandom(10, 1000);
+        sortOnRandom(10, 10000);
+        sortOnRandom(10, 100000);
+        sortOnRandom(10, 1000000);
+    }
+
+    public void testOnSameArray() {
+        sortOnSame(10,10);
+        sortOnSame(10,100);
+        sortOnSame(10,1000);
+        sortOnSame(10,10000);
+        sortOnSame(10,100000);
+    }
+
+    void sortOnSame(int n, int nn) {// n ilosc powtorzen nn ilosc elementow
+        double avgTime = 0;
+        long startTime;
+        long sortTime;
+        int baseArray[] = new int[nn];
+        randomFill(baseArray);
+        System.out.println("Ilość elementów : " + nn + ", ");
+        for (int s = 0; s < 4; s++) {
+            switch (s) {
+                case 0:
+                    setSortStrategy(new JavaSortStrategy());
+                    System.out.print("Arrays.sort() ");
+                    break;
+                case 1:
+                    setSortStrategy(new QuickSortStrategy());
+                    System.out.print("QuickSort ");
+                    break;
+                case 2:
+                    setSortStrategy(new BubbleSortStrategy());
+                    System.out.print("BubbleSort ");
+                    break;
+                case 3:
+                    setSortStrategy(new SelectionSortStrategy());
+                    System.out.print("SelectionSort ");
+                    break;
+            }
+            for (int i = 0; i < n; i++) {
+                int array[] = Arrays.copyOf(baseArray, baseArray.length);
+                startTime = System.nanoTime();
+                strategy.sort(array);
+                sortTime = System.nanoTime() - startTime;
+                avgTime += sortTime;
+            }
+            avgTime /= n;
+            System.out.println("Sredni czas: " + (double) avgTime / 1_000_000_000 + " sekund.");
+        }
+        System.out.println("");
+    }
+
+    void sortOnRandom(int n, int nn) {// n ilosc powtorzen nn ilosc elementow
+        double avgTime = 0;
+        long startTime;
+        long sortTime;
+
+        System.out.print("Ilość elementów : " + nn + ", ");
+        for (int i = 0; i < n; i++) {
+            int array[] = new int[nn];
+            randomFill(array);
+            startTime = System.nanoTime();
+            strategy.sort(array);
+            sortTime = System.nanoTime() - startTime;
+            avgTime += sortTime;
+        }
+        avgTime /= n;
+        System.out.println("Sredni czas: " + (double) avgTime / 1_000_000_000 + " sekund.");
+
+    }
+
+    void randomFill(int array[]) {
+        Random random = new Random();
+        for (int i = 0; i < array.length; i++) {
+            array[i] = (int) (random.nextDouble() * array.length * 10);
+        }
+    }
+
+}
--- Strategia/src/strategia/Strategia.java
+++ Strategia/src/strategia/Strategia.java
@@ -1,54 +1,12 @@
-
 package strategia;
 
 public class Strategia {
 
     public static void main(String[] args) {
-        
-        Sorterer sorterer = new Sorterer();
-        
-        System.out.println("Sortowanie QuickSort");
-        sorterer.setSortStrategy(new QuickSortStrategy());
-        sorterer.testSort(10,10);
-        sorterer.testSort(10,100);
-        sorterer.testSort(10,1000);
-        sorterer.testSort(10,10000);
-        sorterer.testSort(10,100000);
-        sorterer.testSort(10,1000000); 
-        
-        System.out.println("");
-        
-        System.out.println("Sortowanie Arrays.sort()");
-        sorterer.setSortStrategy(new JavaSortStrategy());
-        sorterer.testSort(10,10);
-        sorterer.testSort(10,100);
-        sorterer.testSort(10,1000);
-        sorterer.testSort(10,10000);
-        sorterer.testSort(10,100000);
-        sorterer.testSort(10,1000000); 
-        
-        
-        System.out.println("");
-        
-        System.out.println("Sortowanie BubbleSort");
-        sorterer.setSortStrategy(new BubbleSortStrategy());
-        sorterer.testSort(10,10);
-        sorterer.testSort(10,100);
-        sorterer.testSort(10,1000);
-        sorterer.testSort(10,10000);
-        sorterer.testSort(10,100000);
-        sorterer.testSort(10,1000000); 
-        
-          System.out.println("");
-        
-        System.out.println("Sortowanie SelectionSort");
-        sorterer.setSortStrategy(new SelectionSortStrategy());
-        sorterer.testSort(10,10);
-        sorterer.testSort(10,100);
-        sorterer.testSort(10,1000);
-        sorterer.testSort(10,10000);
-        sorterer.testSort(10,100000);
-        sorterer.testSort(10,1000000); 
+
+        Sorter sorter = new Sorter();
+        sorter.testOnRandomArrays();
+        //sorter.testOnSameArray();
     }
-    
+
 }
